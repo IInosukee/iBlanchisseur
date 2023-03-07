94fd8c85fc87a2e921cb24d9ba28eaa02794a6b9
IInosukee <83782101+IInosukee@users.noreply.github.com>
2023-03-07 22:00:54 +0100

    Update server.lua

--- iBlanchisseur/server.lua
+++ iBlanchisseur/server.lua
@@ -6,11 +6,15 @@ AddEventHandler("blanchir_argent", function(gain)
     local _src = source
     local xPlayer = ESX.GetPlayerFromId(_src)
     local taxes = Config.Taxes
-
-    total = gain*taxes
-    xPlayer.removeAccountMoney("black_money", gain)
-    TriggerClientEvent("esx:showAdvancedNotification", _src, "informations", "blanchiment", "ça sera blanchi dans ~r~"..math.floor(Config.Time/1000).."~s~ secondes")
-    Citizen.Wait(Config.Time)
-    xPlayer.addMoney(total)
-    TriggerClientEvent("esx:showAdvancedNotification", _src, "informations", "blanchiment", "Tu as reçu ~g~"..ESX.Math.Round(total).."$")
+    
+    if gain > 0 and xPlayer.getAccount('black_money').money >= gain then
+        total = gain*taxes
+        xPlayer.removeAccountMoney("black_money", gain)
+        TriggerClientEvent("esx:showAdvancedNotification", _src, "informations", "blanchiment", "Ça sera blanchi dans ~r~"..math.floor(Config.Time/1000).."~s~ secondes")
+        Citizen.Wait(Config.Time)
+        xPlayer.addMoney(total)
+        TriggerClientEvent("esx:showAdvancedNotification", _src, "informations", "blanchiment", "Tu as reçu ~g~"..ESX.Math.Round(total).."$")
+    else
+        TriggerClientEvent("esx:showAdvancedNotification", _src, "informations", "blanchiment", "Reviens me voir quand tu aura assez d'argent !")
+    end
 end)
